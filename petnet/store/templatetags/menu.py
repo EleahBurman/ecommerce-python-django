1043b4109ae18521a3f3fe4960de123a18a37939
eleahburman <eleahjburman@gmail.com>
2024-03-29 11:13:42 -0400

    Populate category titles in nav bar

--- petnet/store/templatetags/menu.py
+++ petnet/store/templatetags/menu.py
@@ -1,8 +1,9 @@
 from django import template
-
+from store.models import Category
 register = template.Library()
 
 @register.inclusion_tag('core/menu.html')
 
 def menu():
-    return {}
+  categories = Category.objects.all()
+  return {'categories': categories}
